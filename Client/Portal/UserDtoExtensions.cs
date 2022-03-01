2cc8a47587b0e9955ab0f2524dc550886dc86547
Robert Sundström <robert.sundstrom@outlook.com>
2022-03-01 18:04:29 +0100

    Fix Users DisplayName

---
+++ Client/Portal/UserDtoExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using Skynet.IdentityService.Client;
+
+namespace Skynet.Portal;
+
+public static class UserDtoExtensions
+{
+    public static string? GetDisplayName(this Skynet.Client.UserDto user) => !String.IsNullOrEmpty(user.DisplayName) ? user?.DisplayName : $"{user.FirstName} {user?.LastName}";
+
+    public static string? GetDisplayName(this UserDto user) => !String.IsNullOrEmpty(user.DisplayName) ? user?.DisplayName : $"{user.FirstName} {user?.LastName}";
+}
