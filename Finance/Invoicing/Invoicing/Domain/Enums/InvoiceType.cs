4fee8aa057e078d0fdb840125a30ba582e3a05f7
Marina Sundström <marna.li@outlook.com>
2022-07-11 21:21:44 +0200

    Invoice type

--- Finance/Invoicing/Invoicing/Domain/Enums/InvoiceType.cs
+++ Finance/Invoicing/Invoicing/Domain/Enums/InvoiceType.cs
@@ -3,7 +3,7 @@ namespace YourBrand.Invoicing.Domain.Enums;
 [Newtonsoft.Json.JsonConverter(typeof(Newtonsoft.Json.Converters.StringEnumConverter))]
 public enum InvoiceType 
 {
-    Invoice = 1,
-    Cash = 2,
-    Credit = 3
+    Invoice = 0,
+    Cash = 1,
+    Credit = 2
 }
