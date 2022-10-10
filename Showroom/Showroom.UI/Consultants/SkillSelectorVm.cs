6072c7dc10bc146ef066e92046825d6a469507c6
Marina Sundström <marna.li@outlook.com>
2022-10-10 11:40:11 +0200

    Skill selector component

---
+++ Showroom/Showroom.UI/Consultants/SkillSelectorVm.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+
+using YourBrand.Showroom.Client;
+
+namespace YourBrand.Showroom.Consultants;
+
+
+    public class SkillSelectorVm 
+    {
+        [Required]
+        public SkillDto? Skill { get; set; }
+
+        [Required]
+        public SkillLevel? SkillLevel { get; set; } = Showroom.Client.SkillLevel.Competent;
+
+        public List<SkillVM> Skills { get; set; } = new List<SkillVM>();
+
+        public void AddSkill() 
+        {
+            Skills.Add(new SkillVM() {
+                Skill = Skill!,
+                Level = SkillLevel.GetValueOrDefault(),
+                Selected = true
+            });
+
+            Skill = null;
+            SkillLevel = Showroom.Client.SkillLevel.Competent;
+        }
+
+        public void RemoveSkill(SkillVM skill) 
+        {
+            Skills.Remove(skill);
+        }
+    }
+
+    public class SkillVM
+    {
+        public SkillDto Skill { get; set; }
+
+        public SkillLevel Level { get; set; } = Showroom.Client.SkillLevel.Competent;
+
+        public bool Selected { get; set; }
+    }
