80f1faba66098c57fe4085680e5bf110524b99e1
Marina Sundström <marna.li@outlook.com>
2022-10-15 16:22:04 +0200

    Inventory notification

--- Inventory/Inventory/Application/Warehouse/Items/Events/WarehouseItemEventHandler.cs
+++ Inventory/Inventory/Application/Warehouse/Items/Events/WarehouseItemEventHandler.cs
@@ -1,11 +1,10 @@
-using YourBrand.Inventory.Application.Common.Models;
 using YourBrand.Inventory.Domain;
 using YourBrand.Inventory.Domain.Events;
-
-using MediatR;
+using YourBrand.Inventory.Application.Common.Interfaces;
+using YourBrand.Notifications.Contracts;
+using MassTransit;
 
 using Microsoft.EntityFrameworkCore;
-using YourBrand.Inventory.Application.Common.Interfaces;
 
 namespace YourBrand.Inventory.Application.Warehouses.Items.Events;
 
@@ -17,10 +16,12 @@ public class WarehouseItemEventHandler
   IDomainEventHandler<WarehouseItemQuantityAvailableUpdated>
 {
     private readonly IInventoryContext _context;
+    private readonly IPublishEndpoint _publishEndpoint;
 
-    public WarehouseItemEventHandler(IInventoryContext context)
+    public WarehouseItemEventHandler(IInventoryContext context, IPublishEndpoint publishEndpoint)
     {
         _context = context;
+        _publishEndpoint = publishEndpoint;
     }
 
     public async Task Handle(WarehouseItemCreated notification, CancellationToken cancellationToken)
@@ -29,17 +30,6 @@ public class WarehouseItemEventHandler
 
     public async Task Handle(WarehouseItemQuantityOnHandUpdated notification, CancellationToken cancellationToken)
     {
-        var item = await _context.WarehouseItems
-            .FirstOrDefaultAsync(i => i.Id == notification.ItemId);
-
-        if(item is not null) 
-        {
-            if(notification.Quantity <= item.QuantityThreshold  
-                && notification.OldQuantity > item.QuantityThreshold) 
-            {
-                // Ha fallen below threshold
-            }
-        }
     }
 
     public async Task Handle(WarehouseItemsPicked notification, CancellationToken cancellationToken)
@@ -52,5 +42,29 @@ public class WarehouseItemEventHandler
 
     public async Task Handle(WarehouseItemQuantityAvailableUpdated notification, CancellationToken cancellationToken)
     {
+        var item = await _context.WarehouseItems
+            .Include(x => x.Item)
+            .Include(x => x.Warehouse)
+            .ThenInclude(x => x.Site)
+            .FirstOrDefaultAsync(i => i.Id == notification.ItemId);
+
+        if(item is not null) 
+        {
+            if(notification.Quantity <= item.QuantityThreshold  
+                && notification.OldQuantity > item.QuantityThreshold) 
+            {
+                var subject = $"Quantity available threshold for {item.Item.Name} ({item.Item.Id})";
+
+                var body = @$"
+                Quantity available is below threshold.<br />
+                <br />
+                <b>Item:</b> {item.Item.Name} ({item.Item.Id})<br />
+                <b>Warehouse:</b>  {item.Warehouse.Name}<br />
+                <b>Site:</b>  {item.Warehouse.Site.Name}<br />
+                <b>Quantity available:</b>  {item.QuantityAvailable}<br />
+                ";
+                await _publishEndpoint.Publish(new SendEmail("test@email.com", subject, body));
+            }
+        }
     }
 }
