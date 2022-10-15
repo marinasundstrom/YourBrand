44c296d426057c16a490b9c4267a7240164c51ca
Marina Sundström <marna.li@outlook.com>
2022-10-15 15:20:04 +0200

    Update seed

--- Inventory/Inventory/Infrastructure/Persistence/SeedData.cs
+++ Inventory/Inventory/Infrastructure/Persistence/SeedData.cs
@@ -17,9 +17,34 @@ public class SeedData
 
             if (!context.Items.Any())
             {
-                var person = new Item("TS-B-L", "T-Short Blue Larger", string.Empty, string.Empty);
+                var clothes = new ItemGroup("Clothes");
+                context.ItemGroups.Add(clothes);
 
-                context.Items.Add(person);
+                var t1 = new Item("TS-B-S", "T-Shirt Blue Small", clothes.Id, "pc");
+                var t2 = new Item("TS-B-M", "T-Shirt Blue Medium", clothes.Id, "pc");
+                var t3 = new Item("TS-B-L", "T-Shirt Blue Large", clothes.Id, "pc");
+
+                context.Items.Add(t1);
+                context.Items.Add(t2);
+                context.Items.Add(t3);
+
+                await context.SaveChangesAsync();
+
+                var site = new Site("main-site", "Main site");
+                context.Sites.Add(site);
+
+                var warehouse = new Warehouse("main-warehouse", "Main warehouse", site.Id);
+                context.Warehouses.Add(warehouse);
+
+                await context.SaveChangesAsync();
+
+                var wt1 = new WarehouseItem(t1.Id, warehouse.Id, "1-2-3", 100);
+                var wt2 = new WarehouseItem(t2.Id, warehouse.Id,"1-2-4", 100);
+                var wt3 = new WarehouseItem(t3.Id, warehouse.Id, "1-2-5", 100);
+
+                context.WarehouseItems.Add(wt1);
+                context.WarehouseItems.Add(wt2);
+                context.WarehouseItems.Add(wt3);
 
                 await context.SaveChangesAsync();
             }
