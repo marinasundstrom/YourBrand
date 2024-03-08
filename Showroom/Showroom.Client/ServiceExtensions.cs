1186fc2c4f4900b911cb04a19e43203a8ae2b795
Marina <marna.li@outlook.com>
2024-03-09 00:33:38 +0100

    Fix

--- Showroom/Showroom.Client/ServiceExtensions.cs
+++ Showroom/Showroom.Client/ServiceExtensions.cs
@@ -9,35 +9,35 @@ public static class ServiceExtensions
     public static IServiceCollection AddShowroomClients(this IServiceCollection services, Action<IServiceProvider, HttpClient> configureClient, Action<IHttpClientBuilder> builder)
     {
         builder(
-            services.AddHttpClient(nameof(IPersonProfilesClient), configureClient)
+            services.AddHttpClient("Showroom-" + nameof(IPersonProfilesClient), configureClient)
             .AddTypedClient<IPersonProfilesClient>((http, sp) => new PersonProfilesClient(http)));
 
         builder(
-            services.AddHttpClient(nameof(IOrganizationsClient), configureClient)
+            services.AddHttpClient("Showroom-" + nameof(IOrganizationsClient), configureClient)
             .AddTypedClient<IOrganizationsClient>((http, sp) => new OrganizationsClient(http)));
 
         builder(
-            services.AddHttpClient(nameof(ICompetenceAreasClient), configureClient)
+            services.AddHttpClient("Showroom-" + nameof(ICompetenceAreasClient), configureClient)
             .AddTypedClient<ICompetenceAreasClient>((http, sp) => new CompetenceAreasClient(http)));
 
         builder(
-            services.AddHttpClient(nameof(ISkillsClient), configureClient)
+            services.AddHttpClient("Showroom-" + nameof(ISkillsClient), configureClient)
             .AddTypedClient<ISkillsClient>((http, sp) => new SkillsClient(http)));
 
         builder(
-            services.AddHttpClient(nameof(ISkillAreasClient), configureClient)
+            services.AddHttpClient("Showroom-" + nameof(ISkillAreasClient), configureClient)
             .AddTypedClient<ISkillAreasClient>((http, sp) => new SkillAreasClient(http)));
 
         builder(
-            services.AddHttpClient(nameof(ICasesClient), configureClient)
+            services.AddHttpClient("Showroom-" + nameof(ICasesClient), configureClient)
             .AddTypedClient<ICasesClient>((http, sp) => new CasesClient(http)));
 
         builder(
-            services.AddHttpClient(nameof(ICompaniesClient), configureClient)
+            services.AddHttpClient("Showroom-" + nameof(ICompaniesClient), configureClient)
             .AddTypedClient<ICompaniesClient>((http, sp) => new CompaniesClient(http)));
 
         builder(
-            services.AddHttpClient(nameof(IIndustriesClient), configureClient)
+            services.AddHttpClient("Showroom-" + nameof(IIndustriesClient), configureClient)
             .AddTypedClient<IIndustriesClient>((http, sp) => new IndustriesClient(http)));
 
         return services;
