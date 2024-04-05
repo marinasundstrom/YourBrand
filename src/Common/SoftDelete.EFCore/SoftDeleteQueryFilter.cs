b0f3a5305fea92ca68304cec7dd1236762b41855
Marina <marna.li@outlook.com>
2024-04-06 00:07:01 +0200

    Cache query filter

--- src/Common/SoftDelete.EFCore/SoftDeleteQueryFilter.cs
+++ src/Common/SoftDelete.EFCore/SoftDeleteQueryFilter.cs
@@ -7,12 +7,18 @@ public static class SoftDeleteQueryFilter
 {
     static readonly Type softDeleteInterface = typeof(ISoftDelete);
     static readonly PropertyInfo deletedProperty = softDeleteInterface.GetProperty(nameof(ISoftDelete.Deleted));
+    private static Expression<Func<ISoftDelete, bool>> expression;
 
     public static Expression<Func<ISoftDelete, bool>>? GetFilter()
     {
-        var param = Expression.Parameter(softDeleteInterface, "entity");
-        var body = Expression.Equal(Expression.Property(param, deletedProperty!), Expression.Constant(null));
-        return Expression.Lambda<Func<ISoftDelete, bool>>(body, param);
+        if (expression is null)
+        {
+            var param = Expression.Parameter(softDeleteInterface, "entity");
+            var body = Expression.Equal(Expression.Property(param, deletedProperty!), Expression.Constant(null));
+            expression = Expression.Lambda<Func<ISoftDelete, bool>>(body, param);
+        }
+        
+        return expression;
     }
 
     public static bool CanApplyTo(Type entityType) => softDeleteInterface.IsAssignableFrom(entityType);
