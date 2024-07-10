4b70c5650206095e8d2553b9e8f0317895a180db
Marina <marna.li@outlook.com>
2024-07-10 22:10:03 +0200

    Update AppHost

--- src/YourBrand.AppHost/Program.cs
+++ src/YourBrand.AppHost/Program.cs
@@ -43,13 +43,17 @@ var appserviceDb = sqlServer.AddDatabase("appserviceDb", "AppService");
 var appservice = builder.AddProject<AppService>("appservice")
     .WithReference(appserviceDb)
     .WithReference(messaging)
-    .WithReference(blobStorage);
+    .WithReference(blobStorage)
+    .WaitFor(appserviceDb)
+    .WaitFor(messaging);
 
 var identityManagementDb = sqlServer.AddDatabase("identityManagementDb", "IdentityManagement");
 var identityManagement = builder.AddProject<IdentityManagement>("identityManagement")
     .WithReference(identityManagementDb)
     .WithReference(messaging)
-    .WithReference(blobStorage);
+    .WithReference(blobStorage)
+    .WaitFor(identityManagementDb)
+    .WaitFor(messaging);
 
 var notificationsDb = sqlServer.AddDatabase("notificationsDb", "Notifications");
 var notifications = builder.AddProject<Notifications>("notifications")
@@ -57,128 +61,165 @@ var notifications = builder.AddProject<Notifications>("notifications")
     .WithReference(hangfireDb)
     .WithReference(messaging)
     .WithReference(blobStorage)
-    .WaitOn(hangfireDb);
+    .WaitFor(hangfireDb)
+    .WaitFor(notificationsDb)
+    .WaitFor(messaging);
 
 var salesDb = sqlServer.AddDatabase("salesDb", "Sales");
 var sales = builder.AddProject<Sales>("sales")
     .WithReference(salesDb)
     .WithReference(messaging)
-    .WithReference(blobStorage);
+    .WithReference(blobStorage)
+    .WaitFor(salesDb)
+    .WaitFor(messaging);
 
 var catalogDb = sqlServer.AddDatabase("catalogDb", "Catalog");
 var catalog = builder.AddProject<Catalog>("catalog")
     .WithReference(catalogDb)
     .WithReference(messaging)
-    .WithReference(blobStorage);
+    .WithReference(blobStorage)
+    .WaitFor(catalogDb)
+    .WaitFor(messaging);
 
 var customersDb = sqlServer.AddDatabase("customersDb", "Customers");
 var customers = builder.AddProject<Customers>("customers")
     .WithReference(customersDb)
     .WithReference(messaging)
-    .WithReference(blobStorage);
+    .WithReference(blobStorage)
+    .WaitFor(customersDb)
+    .WaitFor(messaging);
 
 var ticketingDb = sqlServer.AddDatabase("ticketingDb", "Ticketing");
 var ticketing = builder.AddProject<Ticketing>("ticketing")
     .WithReference(ticketingDb)
     .WithReference(messaging)
-    .WithReference(blobStorage);
+    .WithReference(blobStorage)
+    .WaitFor(ticketingDb)
+    .WaitFor(messaging);
 
 var humanresourcesDb = sqlServer.AddDatabase("humanresourcesDb", "HumanResources");
 var humanresources = builder.AddProject<HumanResources>("humanresources")
     .WithReference(humanresourcesDb)
     .WithReference(messaging)
-    .WithReference(blobStorage);
+    .WithReference(blobStorage)
+    .WaitFor(humanresourcesDb)
+    .WaitFor(messaging);
 
 var marketingDb = sqlServer.AddDatabase("marketingDb", "Marketing");
 var marketing = builder.AddProject<Marketing>("marketing")
     .WithReference(marketingDb)
     .WithReference(messaging)
-    .WithReference(blobStorage);
+    .WithReference(blobStorage)
+    .WaitFor(marketingDb)
+    .WaitFor(messaging);
 
 var inventoryDb = sqlServer.AddDatabase("inventoryDb", "Inventory");
 var inventory = builder.AddProject<Inventory>("inventory")
-.WithReference(inventoryDb)
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(inventoryDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WaitFor(inventoryDb)
+    .WaitFor(messaging);
 
 //
 
 var identityserviceDb = sqlServer.AddDatabase("identityserviceDb", "AspIdUsers");
 var identityService = builder.AddProject<IdentityService>("identityservice")
-.WithReference(identityserviceDb)
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(identityserviceDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WaitFor(identityserviceDb)
+    .WaitFor(messaging);
 
 //
 
 var timereportDb = sqlServer.AddDatabase("timereportDb", "TimeReport");
 var timereport = builder.AddProject<TimeReport>("timereport")
-.WithReference(timereportDb)
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(timereportDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WaitFor(timereportDb)
+    .WaitFor(messaging);
 
 var showroomDb = sqlServer.AddDatabase("showroomDb", "Showroom");
 var showroom = builder.AddProject<Showroom>("showroom")
-.WithReference(showroomDb)
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(showroomDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WaitFor(showroomDb)
+    .WaitFor(messaging);
 
 var accountingDb = sqlServer.AddDatabase("accountingDb", "Accounting");
 var accounting = builder.AddProject<Accounting>("accounting")
-.WithReference(accountingDb)
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(accountingDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WaitFor(accountingDb);
 
 var transactionsDb = sqlServer.AddDatabase("transactionsDb", "Transactions");
 var transactions = builder.AddProject<Transactions>("transactions")
-.WithReference(transactionsDb)
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(transactionsDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WaitFor(transactionsDb)
+    .WaitFor(messaging);
 
 var paymentsDb = sqlServer.AddDatabase("paymentsDb", "Payments");
 var payments = builder.AddProject<Payments>("payments")
-.WithReference(paymentsDb)
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(paymentsDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WaitFor(paymentsDb)
+    .WaitFor(messaging);
 
 var invoicingDb = sqlServer.AddDatabase("invoicingDb", "Invoicing");
 var invoicing = builder.AddProject<Invoicing>("invoicing")
-.WithReference(invoicingDb)
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(invoicingDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WaitFor(invoicingDb)
+    .WaitFor(messaging);
 
 var rotRutServiceDb = sqlServer.AddDatabase("rotRutServiceDb", "RotRutService");
 var rotRutService = builder.AddProject<RotRutService>("rotRutService")
-.WithReference(rotRutServiceDb)
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(rotRutServiceDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WaitFor(rotRutServiceDb)
+    .WaitFor(messaging);
 
 var documentsDb = sqlServer.AddDatabase("documentsDb", "Documents");
 var documents = builder.AddProject<Documents>("documents")
-.WithReference(documentsDb)
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(documentsDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WaitFor(documentsDb)
+    .WaitFor(messaging);
 
 var chatAppDb = sqlServer.AddDatabase("chatAppDb", "ChatApp");
 var chatApp = builder.AddProject<ChatApp>("chatApp")
-.WithReference(chatAppDb)
-.WithReference(messaging)
-.WithReference(blobStorage)
-.WithReference(redis);
+    .WithReference(chatAppDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WithReference(redis)
+    .WaitFor(chatAppDb)
+    .WaitFor(messaging);
 
 var analyticsDb = sqlServer.AddDatabase("analyticsDb", "Analytics");
 var analytics = builder.AddProject<Analytics>("analytics")
-.WithReference(analyticsDb)
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(analyticsDb)
+    .WithReference(messaging)
+    .WithReference(blobStorage)
+    .WaitFor(analyticsDb)
+    .WaitFor(messaging);
 
 var storefront = builder.AddProject<StoreFront>("storeFront")
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(messaging)
+    .WithReference(blobStorage);
 
 var store = builder.AddProject<Store>("store")
-.WithReference(messaging)
-.WithReference(blobStorage);
+    .WithReference(messaging)
+    .WithReference(blobStorage);
 
 builder.AddYarp("ingress")
        .WithEndpoint(port: 5174, scheme: "https")
