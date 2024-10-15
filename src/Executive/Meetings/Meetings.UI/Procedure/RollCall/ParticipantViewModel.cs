3555eef7678e93f9853cfec2058c6c2cac2dee56
Marina <marna.li@outlook.com>
2024-10-15 17:09:57 +0200

    Roll call

---
+++ src/Executive/Meetings/Meetings.UI/Procedure/RollCall/ParticipantViewModel.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace YourBrand.Meetings.Procedure.RollCall;
+
+public class ParticipantViewModel
+{
+    public string Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public ParticipantRole Role { get; set; } = ParticipantRole.Participant;
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    public Portal.User User { get; set; }
+
+    public bool HasVotingRights { get; set; }
+
+    public bool IsPresent { get; set; }
+
+    public ParticipantViewModel Clone()
+    {
+        return new ParticipantViewModel
+        {
+            Name = Name,
+            Role = Role,
+            Email = Email,
+            User = User,
+            HasVotingRights = HasVotingRights,
+            IsPresent = IsPresent
+        };
+    }
+}
