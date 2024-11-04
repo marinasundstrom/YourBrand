f50baa4b310edf861ab825b5afc1b3394481d664
Marina <marna.li@outlook.com>
2024-11-04 11:33:29 +0100

    Modules page

--- src/AppService/Application/Modules/TenantModules/PopulateModulesQuery.cs
+++ src/AppService/Application/Modules/TenantModules/PopulateModulesQuery.cs
@@ -6,15 +6,19 @@ using Microsoft.EntityFrameworkCore;
 using YourBrand.Application.Common.Interfaces;
 using YourBrand.Domain.Entities;
 using YourBrand.Identity;
+using YourBrand.Tenancy;
 
 namespace YourBrand.Application.Modules.TenantModules;
 
 public record PopulateModulesQuery() : IRequest
 {
-    public sealed class Handler(IAppServiceContext appServiceContext) : IRequestHandler<PopulateModulesQuery>
+    public sealed class Handler(IAppServiceContext appServiceContext, ITenantContext tenantContext) : IRequestHandler<PopulateModulesQuery>
     {
         public async Task Handle(PopulateModulesQuery request, CancellationToken cancellationToken)
         {
+            if(appServiceContext.TenantModules.Any(x => x.TenantId == tenantContext.TenantId))
+                return;
+
             var tenantModules = appServiceContext.Modules.Select(module => new TenantModule()
             {
                 Module = module,
