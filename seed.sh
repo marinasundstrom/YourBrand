67e53625588fa3fe9cc2cf0400cb34d0fc54b957
Marina Sundström <marna.li@outlook.com>
2025-01-12 12:14:59 +0100

    Add comment

--- seed.sh
+++ seed.sh
@@ -1,3 +1,5 @@
+# Run this when AppHost is running. It will seed essential services.
+
 dotnet run --project ./src/IdentityManagement/IdentityManagement/IdentityManagement.csproj -- --seed
 dotnet run --project ./src/AppService/WebApi/WebApi.csproj -- --seed
 dotnet run --project ./src/Notifications/Notifications/Notifications.csproj -- --seed
