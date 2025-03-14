9a124051f1aa2d630622ca33d29d76356408a1cc
Marina Sundström <marna.li@outlook.com>
2025-03-14 21:31:10 +0100

    Update theme of Store

--- src/Store/Web/Server/wwwroot/js/app.js
+++ src/Store/Web/Server/wwwroot/js/app.js
@@ -1,7 +1,7 @@
 window.getCid = () => JSON.parse(localStorage.getItem("cid") || "");
 
 window.scrollToTop = () => {
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
 };
 
 function initScrollToTop() {
